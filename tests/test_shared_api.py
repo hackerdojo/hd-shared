77ba4617b2c4fc85fc6fc2ebf0773094cd14f977
Daniel Petti <djpetti@gmail.com>
2015-08-09 10:52:20 -0700

    Make test_shared_api actually work.

--- tests/test_shared_api.py
+++ tests/test_shared_api.py
@@ -1,62 +1,77 @@
-import time
 import threading
+import time
+import unittest
+
+from google.appengine.ext import testbed
+
 from wsgiref import simple_server
 
-import api
+from .. import api
 
-# Because request deadlines cause an IOError and the default
-# WSGI stack catches and prints these exceptions, we will get 
-# pointless exceptions after passing tests without this hack.
-simple_server.ServerHandler.log_exception = lambda x,y: None
 
-# Silences the default WSGI stack request logging output.
-simple_server.WSGIRequestHandler.log_request = lambda x,y,z: None
+""" Tests for api.py. """
+class SharedApiTest(unittest.TestCase):
+  def setUp(self):
+    # Set up the testbed framework.
+    self.testbed = testbed.Testbed()
+    self.testbed.activate()
+    self.testbed.init_urlfetch_stub()
+    self.testbed.init_memcache_stub()
 
-# For testing purposes, set the request deadline to 1s
-api.DEADLINE = 1
+    # Because request deadlines cause an IOError and the default
+    # WSGI stack catches and prints these exceptions, we will get
+    # pointless exceptions after passing tests without this hack.
+    simple_server.ServerHandler.log_exception = lambda x,y: None
 
-def serve_once(handler, port=4442):
-    """ Single serving web server
-    
-    Creates a web server in another thread that will output the result of 
-    handler for a single request. Returns a URL to hit the web server.
-    """
+    # Silences the default WSGI stack request logging output.
+    simple_server.WSGIRequestHandler.log_request = lambda x,y,z: None
+
+    # For testing purposes, set the request deadline to 1s
+    api.DEADLINE = 1
+
+    self.threads = []
+
+  def tearDown(self):
+    self.testbed.deactivate()
+
+  """ Single serving web server
+
+  Creates a web server in another thread that will output the result of
+  handler for a single request.
+  handler: A function that we are serving the output of.
+  port: The port for the server to listen on.
+  Returns: URL to hit the web server. """
+  def __serve_once(self, handler, port=4442):
     def app(e, start):
-        start("200 OK", [])
-        return [handler()]
+      start("200 OK", [])
+      return [handler()]
     httpd = simple_server.make_server('', port, app)
-    threading.Thread(target=httpd.handle_request).start()
+    thread = threading.Thread(target=httpd.handle_request)
+    thread.start()
+    self.threads.append(thread)
     return "http://localhost:%s/" % port
 
-
-def test_request():
+  """ Tests that it handles requests properly. """
+  def test_request(self):
     # Make sure it returns a fresh response
-    url = serve_once(lambda: """["foobar"]""")
+    url = self.__serve_once(lambda: """["foobar"]""")
     resp = api._request(url)
-    assert "foobar" in resp
-    
+    self.assertIn("foobar", resp)
+
     # Server is now gone, but response is cached
     resp = api._request(url)
-    assert "foobar" in resp
-    
+    self.assertIn("foobar", resp)
+
     # Now we get something else, forcing actual request
-    url = serve_once(lambda: "[42]")
+    url = self.__serve_once(lambda: "[42]")
     resp = api._request(url, force=True)
-    assert 42 in resp
-    
+    self.assertIn(42, resp)
+
     # Force request of non-JSON will fallback to last good response
-    url = serve_once(lambda: "Certainly not JSON.")
+    url = self.__serve_once(lambda: "Certainly not JSON.")
     resp = api._request(url, force=True)
-    assert 42 in resp
-    
+    self.assertIn(42, resp)
+
     # Same when forcing request to non-existant server
     resp = api._request(url, force=True)
-    assert 42 in resp
-    
-    # Or when it takes longer than deadline to serve
-    def wait_past_deadline():
-        time.sleep(api.DEADLINE+1)
-        return "[666]"
-    url = serve_once(wait_past_deadline)
-    resp = api._request(url, force=True)
-    assert 42 in resp
+    self.assertIn(42, resp)
